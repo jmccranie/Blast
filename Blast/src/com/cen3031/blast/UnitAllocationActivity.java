3d8ba47d6cc1456f1e74e9f9aee60ddf28af5789
zhin104 <zhin104@gmail.com>
2013-03-27 18:54:02 -0400

    Deleted some name of some dude...

--- Blast/src/com/cen3031/blast/UnitAllocationActivity.java
+++ Blast/src/com/cen3031/blast/UnitAllocationActivity.java
@@ -34,13 +34,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-/**
- * (c) 2010 Nicolas Gramlich
- * (c) 2011 Zynga
- *
- * @author Nicolas Gramlich
- * @since 15:13:46 - 15.06.2010
- */
 public class UnitAllocationActivity extends SimpleBaseGameActivity implements IOnSceneTouchListener {
 	// ===========================================================
 	// Constants
